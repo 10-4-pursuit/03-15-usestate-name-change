e4a3eb729e28cb647d5be449a12d9886926023d7
MoniqueC10-4 <monique.charles13@gmail.com>
2023-12-20 22:40:26 -0500

    Add NameChanger component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NameChanger from './NameChanger.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NameChanger />
+      {/* name= "Tigger"  */}
+    </div>
+  );
+}
+
+export default App;
