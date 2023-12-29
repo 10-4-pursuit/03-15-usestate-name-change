861b0e2d461e77715920c23caf5df23e5398bf66
Azmain Shoeb <azmainshoeb@pursuit.org>
2023-12-28 20:32:14 -0500

    Add NameChanger component

---
+++ jsx-basic-react-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NameChanger from './NameChanger';
+
+function App() {
+  return (
+    <div className="App">
+      <NameChanger />
+    </div>
+  );
+}
+
+export default App;
