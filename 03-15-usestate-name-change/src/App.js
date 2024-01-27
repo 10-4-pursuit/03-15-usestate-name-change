33a00e23d5e1df199219af8a94cb2110e1e5f152
lanell1 <lanelllopez@pursuit.org>
2024-01-27 15:53:13 -0500

    complete-03-15-usestate-name-change

---
+++ 03-15-usestate-name-change/src/App.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import NameChanger from './NameChanger';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <NameChanger />
+    </div>
+  );
+};
+
+export default App;
+
