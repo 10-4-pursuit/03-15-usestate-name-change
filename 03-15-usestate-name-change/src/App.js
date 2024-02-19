149732b7f7564023c5a88f40206cfc6661144a25
Jpena101 <jannpena@pursuit.org>
2024-02-19 09:57:49 -0500

    Add NameChanger component

---
+++ 03-15-usestate-name-change/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NameChanger from './NameChanger';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello User!</h1>
+      <NameChanger />
+    </div>
+  );
+};
+
+export default App;
